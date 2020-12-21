7c07945dd0d7eab24e8731c54f88b85fb7916182
james kim <phpprogram47@gmail.com>
2020-12-21 16:11:27 +0900

    리그 아이디 가져오기 추가합니다.

--- list.py
+++ list.py
@@ -16,26 +16,47 @@ def get_league_name(soup) :
         if t.find('결과') == 0 : #html 소스중에 결과라는 텍스트가 있으면 while문의 빠져 나간다.
             return False            
 
+def get_league_id(soup) :        
+    league_id = [item["data-category-treeid"] for item in soup.find_all() if "data-category-treeid" in item.attrs]
+    league_id_temp = []
+    for i in league_id:      
+        league_id_temp.append(i.replace('\\"',""))
+
+    return league_id_temp
+# make_json = soup.get_text()
+# json_data = json.loads(make_json)  
+# print(json_data[0]['content'].find("div").attrs["data-category-treeid"])
+# print(json_data[0]['content'])
+
 #여기서 부터 시작 입니다.
 start = 0
 league_name_list = []
+league_id = []
 result_chk = True
 while result_chk == True :
+    # 브라우저 없이 크롬을 실행한다.
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
+    options.add_argument('window-size=1920x1080')
+    options.add_argument("disable-gpu")
     URL = "https://www.marathonbet.com/ko/betting/?page="+str(start)+"&pageAction=getPage"
-    driver = webdriver.Chrome(executable_path='chromedriver')
+    driver = webdriver.Chrome(executable_path='chromedriver', chrome_options=options)
     driver.get(url=URL)
     html_doc = driver.page_source
     soup = BeautifulSoup(html_doc, 'html.parser')
     make_json = soup.get_text()
     json_data = json.loads(make_json)    
     if json_data[1]['val'] == False :
-        break
+        result_chk = False
     else :
         res = get_league_name(soup)       
         if res == False :
             result_chk = False # while문의 빠져 나간다.
+        else :
+            league_id = get_league_id(soup)
+    start+=1    
+    result_chk = False    
 
-    start+=1        
-
-print(league_name_list)
+print(league_id)
+# print(league_name_list)
 driver.close()
